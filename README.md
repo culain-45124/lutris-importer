aff62b1b01dfe226592d21f95a8747f117fe681a
Culain Lach Feragh <culain.l.feragh@outlook.com>
2023-11-02 21:57:56 +0000

    Add script and README

---
+++ README.md
@@ -0,0 +1,64 @@
+# lutris-importer
+
+Lightweight script designed to import games from [Lutris](https://github.com/lutris/lutris) in to Steam along with corresponding art work uing [Steam ROM Manager](https://github.com/SteamGridDB/steam-rom-manager).
+
+Run the command to create a folder in the current working directory for each "service" registered in Lutris. The command will then make a `.json` manifest file containing the details for each game associated with that service in the respective folders.
+
+These `.json` files can then be parsed by Steam Rom Manager to import them in to Steam with the correct artwork.
+
+The manifest files make use of the [Lutris CLI protocol link](https://github.com/lutris/lutris#command-line-options) functionality, using `lutris:` followed by a game identifier, which will open a game via Lutris if it is installed, or install it if not. Unfortuantely this doesn't work for all services currently, see the serivce compatibility table below.
+
+## Installation
+
+Clone the repo using:
+
+`git clone git@github.com:JavanCook/lutris-importer.git`
+
+Create a new bin directory if you don't already have one:
+
+`mkdir ~/bin`
+
+Symlink script in to the bin directory:
+
+`ln -s /home/deck/lutris-importer/lutris-import.sh /home/deck/bin/lutris-import`
+
+Add bin to your path:
+
+`echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc`
+
+Restart your terminal, or open a new tab for changes to take effect
+
+## Usage
+
+Register all the services you want to on Lutris, you need a version >0.5.14 for this to work
+
+Change directory to location you want to create the folders, for example
+
+`cd /run/media/mmcblk0p1/Emulation`
+
+Run the command
+
+`lutris-import`
+
+This should create all the corresponding files and folders
+
+## Options
+
+| Arguments | Description |
+| --------- | ----------- |
+| `-f`      | If the script detects a file with the name of the service in the current working directory it will exit so as not to delete anything important. Use `-f` to force the deletion of this file so it can be replaced with a directory |
+| `-h`      | Display help text |
+
+
+## Import via Steam ROM Manager
+
+You'll need to do this for each service that you want to add the games from to Steam
+
+To import via Steam ROM Manager:
+
+1. Open Steam ROM Manager
+1. Click "Create Parser"
+1. Under "PARSER TYPE" select "Manual"
+1. For "MANIFESTS DIRECTORY" select the service directory that you're adding, e.g. `/run/media/mmcblk0p1/Emulation/gog`
+1. Fill in the other options as required, click "Test parser" to check it works then "Save" to finish
+1. Use the "Preview" pane as usual to select artwork and add to Steam
---
+++ lutris-import.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -euo pipefail
+
+##LOGGING
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+lbl=$'\e[1;94m'
+prp=$'\e[1;35m'
+underline=$'\e[4m'
+end=$'\e[0m'
+
+function logit () {
+  message=$1
+  flair=$2
+  if test -z "${flair}"; then
+    printf "%s\n" " -- ${message}"
+  else
+    printf "%s\n" "${!flair} -- ${message}${end}"
+  fi
+}
+
+## OPTIONS
+force=0
+help=0
+while getopts "fh" opt
+do
+  case ${opt} in
+    f)
+      force=1
+    ;;
+    h)
+      help=1
+    ;;
+    \?)
+      logit "Unknown option: ${opt}" red
+      exit 1
+    ;;
+  esac
+done
+shift "$((OPTIND-1))"
+
+if [[ "${help}" -eq 1 ]]; then
+  logit "desciption message" purp
+  logit " -f : force - Delete files with service names and replace with folders"
+  logit " -h : help - Show this help messsage"
+  exit 0
+fi
+
+games="$(flatpak run net.lutris.Lutris -aj)"
+services="$(echo $games | jq -cr 'map(.service) | unique')"
+for service in $(echo $services | jq -cr '.[]'); do
+    logit "Checking if directory \"$service\" already exists" yel
+    if [[ ! -d "./$service" && -e "./$service" ]]; then
+        if [[ $force == 1 ]]; then
+            logit "Found existing file called \"$service\"" red
+            logit "Removing existing file called \"$service\"" red
+            rm -r ./$service
+        else
+            logit "Found existing file called \"$service\", exiting" red
+            exit 1
+        fi
+    fi
+    if [[ ! -e "./$service" ]]; then
+        logit "Service directory $service not found" red
+        logit "Creating directory \"$service\"" grn
+        mkdir "./$service"
+    fi
+    logit "Checking if manifest file \"$service-manifest.json\" already exists" yel
+    if [[ -e "./$service/$service-manifest.json" ]]; then
+        logit "Found existing manifest file called \"$service-manifest.json\"" red
+        logit "Removing existing manifest file called \"$service-manifest.json\"" red
+        rm -r ./$service/$service-manifest.json
+    fi
+    logit "Creating file \"$service-manifest.json\"" grn
+    touch "./$service/$service-manifest.json"
+    logit "Importing of games from service $service starting" prp
+    echo "[" >> ./$service/$service-manifest.json
+    echo "$games" | jq -cr ".[] | select(.service == \"$service\")" | while read -r game; do
+        game_name=$(echo $game | jq -r '.name')
+        game_details=$(echo $game | jq -r '.details')
+        app_id=$(echo $game_details | jq -r '.id')
+        logit "Adding game $game_name to manifest file \"$service-manifest.json\"" grn
+        echo "  {" >> ./$service/$service-manifest.json
+        echo "    \"title\": \"$game_name\"," >> ./$service/$service-manifest.json
+        echo "    \"target\": \"/usr/bin/flatpak\"," >> ./$service/$service-manifest.json
+        echo "    \"startIn\": \"\"," >> ./$service/$service-manifest.json
+        echo "    \"launchOptions\": \"run net.lutris.Lutris lutris:$service:$app_id\"" >> ./$service/$service-manifest.json
+        echo "  }," >> ./$service/$service-manifest.json
+        added=true
+    done
+    if [[ added ]]; then
+        sed -i '$ d' ./$service/$service-manifest.json
+        echo "  }" >> ./$service/$service-manifest.json
+    fi
+    echo "]" >> ./$service/$service-manifest.json
+    logit "Importing of games from service $service complete" prp
+done
+logit "Importing from Lutris complete" lbl
